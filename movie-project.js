01514fcc2c490808b23aed42cf9a90b4bc89500d
Daniel Name <daniel.v.name@gmail.com>
2022-10-24 14:30:26 -0500

    added omdb and cleaned up repetitive URLs

--- movie-project.js
+++ movie-project.js
@@ -1,34 +1,73 @@
 $(function(){
 
-// objects
-    // crud operators
     const CRUD = {
         Get(){
-            $.get('https://fuchsia-almondine-fisher.glitch.me/movies').done(function(data){
+            $.get(DATA.URL.glitch).done(function(data){
                 console.log(data);
             });
         },
         Post(){
-
+            $.post()
         },
         Push(){
-
+            $.ajax({
+                url: DATA.URL.glitch,
+                method: "PUSH",
+                data: {
+                    // limit:  10,
+                    // offset: 20
+                }
+            }).done(function() {
+                // do something with the data
+            });
         },
         Patch(){
-
+            $.ajax({
+                url: DATA.URL.glitch,
+                method: "PATCH",
+                data: {
+                    // limit:  10,
+                    // offset: 20
+                }
+            }).done(function() {
+                // do something with the data
+            });
         },
         Put(){
-
+            $.ajax({
+                url: DATA.URL.glitch,
+                method: "PUT",
+                data: {
+                    // limit:  10,
+                    // offset: 20
+                }
+            }).done(function() {
+                // do something with the data
+            });
         },
         Delete(){
-            $.delete()
+            $.ajax({
+                url: DATA.URL.glitch,
+                method: "DELETE",
+            }).done(function() {
+                // do something with the data
+            });
         }
     }
-    // init
+
+    const DATA = {
+        URL: {
+            glitch: 'https://fuchsia-almondine-fisher.glitch.me/movies',
+            omdb: `http://www.omdbapi.com/?i=tt3896198&apikey=${omdbKey}`
+        }
+        // i have to have a way to access this from created content
+        // i have to be able to individually tweak any field
+    }
+
     const INIT = {
 
     }
-    // display
+
     const DISPLAY ={
 
     }

781aa3a3cd8108467e9a3e5859da9e2af2f8ef06
Daniel Name <daniel.v.name@gmail.com>
2022-10-24 12:01:11 -0500

    setting up objects to hold functions

--- movie-project.js
+++ movie-project.js
@@ -1,5 +1,37 @@
 $(function(){
-    $.get('https://fuchsia-almondine-fisher.glitch.me/movies').done(function(data){
-        console.log(data);
-    });
+
+// objects
+    // crud operators
+    const CRUD = {
+        Get(){
+            $.get('https://fuchsia-almondine-fisher.glitch.me/movies').done(function(data){
+                console.log(data);
+            });
+        },
+        Post(){
+
+        },
+        Push(){
+
+        },
+        Patch(){
+
+        },
+        Put(){
+
+        },
+        Delete(){
+            $.delete()
+        }
+    }
+    // init
+    const INIT = {
+
+    }
+    // display
+    const DISPLAY ={
+
+    }
+    //
+
 });
